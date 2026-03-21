a81a41b41100435eb04bfe31f3f21413f9fdf03d
Nick Efe Oni <neoni1@sheffield.ac.uk>
2026-03-21 07:16:16 +0000

    feat(terraform): add ecs_cluster_arn output
    Added ecs_cluster_arn output to provide the ARN of the ECS cluster.
    
    This change allows users to easily reference the ECS cluster ARN in other configurations.

--- infrastructure/terraform/envs/dev/outputs.tf
+++ infrastructure/terraform/envs/dev/outputs.tf
@@ -9,3 +9,7 @@ output "athena_database" {
 output "state_machine_arn" {
   value = aws_sfn_state_machine.pipeline.arn
 }
+
+output "ecs_cluster_arn" {
+  value = try(aws_ecs_cluster.analysis[0].arn, null)
+}
