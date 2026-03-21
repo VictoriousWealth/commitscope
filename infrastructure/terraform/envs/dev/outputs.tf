9af1e27b091d52906f668ce3b22771f601099ed4
Nick Efe Oni <neoni1@sheffield.ac.uk>
2026-03-21 08:12:41 +0000

    feat(terraform): add new outputs for lambda and ecs resources
    Added outputs for lambda function name and ECS task definition ARN.
    
    These outputs provide essential information for further integration and deployment.
    
    This change enhances the visibility of key resources in the Terraform configuration.

--- infrastructure/terraform/envs/dev/outputs.tf
+++ infrastructure/terraform/envs/dev/outputs.tf
@@ -10,6 +10,22 @@ output "state_machine_arn" {
   value = aws_sfn_state_machine.pipeline.arn
 }
 
+output "lambda_function_name" {
+  value = aws_lambda_function.pipeline.function_name
+}
+
 output "ecs_cluster_arn" {
   value = try(aws_ecs_cluster.analysis[0].arn, null)
+}
+
+output "ecs_task_definition_arn" {
+  value = try(aws_ecs_task_definition.analysis[0].arn, null)
+}
+
+output "ecr_repository_url" {
+  value = try(aws_ecr_repository.commitscope[0].repository_url, null)
+}
+
+output "effective_container_image_uri" {
+  value = local.effective_container_image_uri
 }
